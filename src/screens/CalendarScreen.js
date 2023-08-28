f03478f2b0eb18867d41cf9e3f3de5314cb733c6
Andi-Tafilaj <tafilajandi@gmail.com>
2023-08-28 16:35:56 -0400

    First Commit

---
+++ src/screens/CalendarScreen.js
@@ -0,0 +1,15 @@
+// src/screens/CalendarScreen.js
+
+import React from 'react';
+import { View } from 'react-native';
+import { Calendar } from 'react-native-calendars';
+
+const CalendarScreen = () => {
+  return (
+    <View>
+      <Calendar />
+    </View>
+  );
+};
+
+export default CalendarScreen;
